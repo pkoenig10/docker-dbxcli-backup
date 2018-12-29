7e7d6fe2e933b0504b94367e2d3dcb0a2c83d326
Patrick Koenig <pkoenig10@gmail.com>
2018-12-28 19:10:14 -0800

    Use UTC timestamps

--- backup.sh
+++ backup.sh
@@ -6,5 +6,5 @@ tmp_file="$(mktemp)"
 tar -czvf $tmp_file $(cat /etc/backup/backup.conf)
 
 name=${NAME:-$(hostname)}
-backup_file="$name-$(date +%Y%m%dT%H%M%S).tar.gz"
+backup_file="${name}_$(date -u +%Y%m%dT%H%M%SZ).tar.gz"
 dbxcli put $tmp_file Backups/$name/$backup_file
